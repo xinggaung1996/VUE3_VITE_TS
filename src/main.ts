c91330f4e9f9c5dd939ae1a0f46bcd29d1218150
= <=>
2022-02-21 19:24:08 +0800

    引入组件

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,13 @@
  * @Author: XG
  * @Date: 2021-10-05 16:37:36
  * @LastEditors: XG
- * @LastEditTime: 2022-02-21 18:46:30
+ * @LastEditTime: 2022-02-21 19:23:46
  */
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index"; // 引入路由
 import "./index.css";
 var a = 5;
-
 const app = createApp(App);
 app.use(router); // 挂载路由到app上
 app.mount("#app");
